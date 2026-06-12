95844c74e80663e7d522f7516f762d4eef9c2f43
Bela Vogler <bela.vogler@uni-jena.de>
2026-06-12 21:59:49 +0200

    oop -> just methods

--- PyFileDialogue/__init__.py
+++ PyFileDialogue/__init__.py
@@ -1,3 +1,16 @@
-from PyFileDialogue.py_file_dialogue import PyFileDialogue
+from PyFileDialogue.py_file_dialogue import (
+    call_directory as call_directory,
+    call_subdirectories as call_subdirectories,
+    call_file as call_file,
+    call_multiple_files as call_multiple_files,
+    call_save_as_file as call_save_as_file,
+)
 
-__all__ = ["PyFileDialogue"]
+
+__all__ = [
+    "call_directory",
+    "call_subdirectories",
+    "call_file",
+    "call_multiple_files",
+    "call_save_as_file",
+]
